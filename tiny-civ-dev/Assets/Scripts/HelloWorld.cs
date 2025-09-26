39ffb2530d1a643f9dc1a24052e03a3490703926
Val Nuñez <val@friendlyhello.com>
2025-09-26 16:39:48 -0700

    vs code setup and adding .meta files to .gitignore
    Testing with hello world, editing a few vs code .json settings and disabling git in editor

---
+++ tiny-civ-dev/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello, world!");
+    }
+}
