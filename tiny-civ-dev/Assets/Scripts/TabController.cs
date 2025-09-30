8f0e229f35723a1c6839d921749dcf8d9212aebe
Val Nuñez <val@friendlyhello.com>
2025-09-29 19:42:25 -0700

    Deleting redundant ProductionPanel script
    I made a better production panel manager

--- tiny-civ-dev/Assets/Scripts/TabController.cs
+++ tiny-civ-dev/Assets/Scripts/TabController.cs
@@ -1,6 +1,37 @@
 using UnityEngine;
+using UnityEngine.UI;
+
+
+// (!) Handles switching between Production and Purchase tabs
 
 public class TabController : MonoBehaviour
 {
-   
+    [Header("Buttons")]
+    [SerializeField] private Button productionButton;
+    [SerializeField] private Button purchaseButton;
+
+    [Header("Panels")]
+    [SerializeField] private GameObject productionPanel;
+    [SerializeField] private GameObject purchasePanel;
+
+    void Start()
+    {
+        productionButton.onClick.AddListener(ShowProduction);
+        purchaseButton.onClick.AddListener(ShowPurchase);
+
+        // Start with Production tab active
+        ShowProduction();
+    }
+
+    private void ShowProduction()
+    {
+        productionPanel.SetActive(true);
+        purchasePanel.SetActive(false);
+    }
+
+    private void ShowPurchase()
+    {
+        productionPanel.SetActive(false);
+        purchasePanel.SetActive(true);
+    }
 }
